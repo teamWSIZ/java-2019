64fc9b24603545e2d2ce52f34cdb9e4bab6c5d43
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-11-09 15:14:28 +0100

    cd..

--- Zajecia5/src/main/java/com/javafx/App.java
+++ Zajecia5/src/main/java/com/javafx/App.java
@@ -14,31 +14,50 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class App extends Application {
+    Rectangle rects[] = new Rectangle[10];
+
+    void addElements(){
+        for(int i=0;i<10;i++) {
+            Rectangle rect = new Rectangle();
+            rect.setWidth(20);
+            rect.setHeight(20);
+            
+            rect.setTranslateX(i*40);
+            rect.setTranslateY(250);
+
+            rect.setFill(Color.BLUE);
+
+            rects[i] = rect;
+        }
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Group group = new Group();
         Scene scene = new Scene(group,500,500);
 
-        for(int i=0;i<10;i++) {
-            Rectangle rect = new Rectangle();
-            rect.setWidth(20);
-            rect.setHeight(20);
+        addElements();
 
-            rect.setTranslateX(i*40);
-            rect.setTranslateY(250);
-
-            rect.setFill(Color.BLUE);
-
-            RotateTransition rt = new RotateTransition();
-            rt.setDuration(Duration.seconds(20));
-            rt.setFromAngle(0);
-            rt.setToAngle(10*360);
-            rt.setNode(rect);
-            rt.playFromStart();
-
-            group.getChildren().add(rect);
+        for(int i=0;i<rects.length;i++){
+            group.getChildren().add(rects[i]);
         }
+
+        double liczby[] = new double[10];
+
+
+        System.out.print("[");
+
+        for(int i=0;i<liczby.length;i++) {
+
+            if(i<liczby.length-1) {
+                System.out.print(liczby[i] + ", ");
+            }else {
+                System.out.print(liczby[i]);
+            }
+
+        }
+
+        System.out.println("]");
 
         primaryStage.setScene(scene);
         primaryStage.show();
