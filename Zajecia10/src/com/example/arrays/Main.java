ceb454762d259668070977cabf673142d063d6db
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-12-21 13:22:11 +0100

    cd..

--- Zajecia10/src/com/example/arrays/Main.java
+++ Zajecia10/src/com/example/arrays/Main.java
@@ -2,14 +2,31 @@ package com.example.arrays;
 
 public class Main {
 
+    public static void wyswietl(double tablica[]){
+        for(int i=0;i<tablica.length;i++)
+            System.out.println("Element["+i+"]="+tablica[i]);
+    }
+
+    public static double sumaElementow(double zmienna[]){
+        double suma = 0;
+
+        for(int i=0;i<zmienna.length;i++){
+            suma = suma + zmienna[i];
+        }
+        return suma;
+    }
+
     public static void main(String[] args) {
-	    double tablica[] = new double[10];
+	    double tablica[] = new double[5];
 
 	    for(int i=0;i<tablica.length;i++)
 	        tablica[i] = i;
 
-	    for(int i=0;i<tablica.length;i++)
-	        System.out.println("Element["+i+"]="+tablica[i]);
+	    wyswietl(tablica);
+
+	    double suma = sumaElementow(tablica);
+
+	    System.out.println("Suma elementów: "+suma);
 
     }
 }
