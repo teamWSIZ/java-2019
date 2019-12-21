2c031c1017db9f9fd4bb70d0ad3d82ffc7091733
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-12-21 12:52:16 +0100

    cd..

--- Zajecia10/src/com/example/arrays/Main.java
+++ Zajecia10/src/com/example/arrays/Main.java
@@ -3,6 +3,13 @@ package com.example.arrays;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    double tablica[] = new double[10];
+
+	    for(int i=0;i<tablica.length;i++)
+	        tablica[i] = i;
+
+	    for(int i=0;i<tablica.length;i++)
+	        System.out.println("Element["+i+"]="+tablica[i]);
+
     }
 }
