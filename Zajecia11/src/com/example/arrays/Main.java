470d03cf96486bf1a23426a662866e95d5453446
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-04 12:44:20 +0100

    cd ..

--- Zajecia11/src/com/example/arrays/Main.java
+++ Zajecia11/src/com/example/arrays/Main.java
@@ -2,7 +2,18 @@ package com.example.arrays;
 
 public class Main {
 
+	public static void wyswietl(double tab[]){
+		for(int i=0;i<tab.length;i++)
+			System.out.println("Element["+i+"]="+tab[i]);
+	}
+
     public static void main(String[] args) {
-	// write your code here
+	    double tablica[] = new double[100];
+
+	    for(int i=0;i<tablica.length;i++)
+	    	tablica[i] = Math.random()*100;
+
+	    System.out.println("Element[0]="+tablica[0]);
+
     }
 }
