3f429fd5b01f1426b2cb4611426bd6954df00203
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-04 14:37:17 +0100

    cd..

--- Zajecia12/src/com/example/arrays/Main.java
+++ Zajecia12/src/com/example/arrays/Main.java
@@ -1,8 +1,23 @@
 package com.example.arrays;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        ArrayList<Integer> tablica = new ArrayList<>();
+
+        tablica.add(20);
+
+        for(int i=0;i<=10;i++)
+            tablica.add((int)Math.pow(i,4));
+
+        tablica.addAll(tablica);
+
+        for(int i=0;i<tablica.size();i++)
+            System.out.println(tablica.get(i));
+
+
     }
 }
