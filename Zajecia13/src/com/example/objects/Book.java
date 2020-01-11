298d70c5088f100cc8ddb68e1314d5ea74045329
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-11 12:47:53 +0100

    cd..

--- Zajecia13/src/com/example/objects/Book.java
+++ Zajecia13/src/com/example/objects/Book.java
@@ -1,5 +1,18 @@
 package com.example.objects;
 
 public class Book {
+    String title;
+    String author;
+    Integer pages;
+    Double price;
 
+    private Book(){
+    }
+
+    Book(String title, String author, Integer pages, Double price){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.price = price;
+    }
 }
--- Zajecia13/src/com/example/objects/Main.java
+++ Zajecia13/src/com/example/objects/Main.java
@@ -3,6 +3,6 @@ package com.example.objects;
 public class Main {
 
     public static void main(String[] args) {
-	    Book book = new Book();
+	    Book book = new Book("Tytuł","Autor",200,50.0);
     }
 }
