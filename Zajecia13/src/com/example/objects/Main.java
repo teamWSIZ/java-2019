0e5e8a0da9ea1431d40dbe23e021c031d595155a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-11 13:23:22 +0100

    cd ..

--- Zajecia13/src/com/example/objects/Main.java
+++ Zajecia13/src/com/example/objects/Main.java
@@ -10,10 +10,22 @@ public class Main {
 	    Book book = new Book("Java","jakis autor",200,50.0);
         Book book1 = new Book("Java1","kolejny autor",250,100.0);
 
-	    System.out.println(book.toString());
-	    System.out.println(book1.toString());
+        books.add(book);
+        books.add(book1);
 
-	    book.print();
-	    book1.print();
+        dodajKsiazki(books);
+
+        for(int i=0;i<books.size();i++){
+        	Book b0 = books.get(i);
+        	b0.print();
+		}
+
+	    for(Book b0 : books){
+	    	b0.print();
+		}
     }
+
+    public static void dodajKsiazki(ArrayList<Book> ksiazki){
+
+	}
 }
