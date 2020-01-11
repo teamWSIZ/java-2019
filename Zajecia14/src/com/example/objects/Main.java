139685eb6438dada5e53458e893f5e071d4fd7e0
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-11 15:11:39 +0100

    cd..

--- Zajecia14/src/com/example/objects/Main.java
+++ Zajecia14/src/com/example/objects/Main.java
@@ -1,6 +1,8 @@
 package com.example.objects;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Main {
 
@@ -9,11 +11,48 @@ public class Main {
 	    dodajLiczby(tablica,10);
 
 	    wyswietlTablice(tablica);
+
+	    tablica.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(o1>o2)
+                    return 1;
+                if(o1<o2)
+                    return -1;
+
+                return 0;
+            }
+        });
+
+        wyswietlTablice(tablica);
+
+        tablica.sort((o1,o2)->{
+            if(o1>o2)
+                return -1;
+            if(o1<o2)
+                return 1;
+
+            return 0;
+        });
+
+        wyswietlTablice(tablica);
+
+        //Nietypowe sortowanie
+        tablica.sort((o1,o2)->{
+            if(o1+o2>o2)
+                return -1;
+            if(o1*o2<o1)
+                return 1;
+
+            return 0;
+        });
+
+        wyswietlTablice(tablica);
     }
 
     public static void dodajLiczby(ArrayList<Integer> tab, int no){
         for(int i=0;i<no;i++)
-            tab.add((int)Math.random()*10);
+            tab.add((int)(Math.random()*10));
     }
 
     public static void wyswietlTablice(ArrayList<Integer> tab){
@@ -21,9 +60,9 @@ public class Main {
 
         for(int i=0;i<tab.size();i++) {
             if (i < tab.size() - 1)
-                System.out.print(i + ", ");
+                System.out.print(tab.get(i) + ", ");
             else
-                System.out.print(i);
+                System.out.print(tab.get(i));
         }
         System.out.println("]");
 
