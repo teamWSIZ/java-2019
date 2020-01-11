80b2d6c783a526c1e04f949b3377119795d42c76
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-11 12:37:46 +0100

    cd..

---
+++ Zajecia13/src/com/example/objects/Book.java
@@ -0,0 +1,5 @@
+package com.example.objects;
+
+public class Book {
+
+}
--- Zajecia13/src/com/example/objects/Main.java
+++ Zajecia13/src/com/example/objects/Main.java
@@ -3,6 +3,6 @@ package com.example.objects;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Book book = new Book();
     }
 }
