7a4fccc3af2c1533dbdaa4ad68bfdcfdc224acc5
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-18 14:02:44 +0100

    cd..

--- Zajecia15/src/com/example/objects/Okno.java
+++ Zajecia15/src/com/example/objects/Okno.java
@@ -1,4 +1,18 @@
 package com.example.objects;
 
 public class Okno {
+    private Double lenght;
+    private Double width;
+
+    private Double area;
+
+    private Okno(){
+    }
+
+    private Okno(Double length, Double width){
+        this.lenght = lenght;
+        this.width = width;
+
+        this.area = lenght*width;
+    }
 }
--- Zajecia15/src/com/example/objects/Student.java
+++ Zajecia15/src/com/example/objects/Student.java
@@ -1,4 +1,18 @@
 package com.example.objects;
 
 public class Student {
+    private String name;
+    private String surname;
+
+    private Integer id;
+
+    private Student(){
+    }
+
+    public Student(String name, String surname, Integer id){
+        this.name = name;
+        this.surname = surname;
+
+        this.id = id;
+    }
 }
