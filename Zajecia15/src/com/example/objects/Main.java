756583067b7f88c909433f49229b6197700e65e1
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-18 14:58:45 +0100

    cd..

--- Zajecia15/src/com/example/objects/Main.java
+++ Zajecia15/src/com/example/objects/Main.java
@@ -4,9 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	    Sala sala41 = new Sala(4,40,20);
-	    Sala sala21 = new Sala(4,40,0);
+	    Sala sala21 = new Sala(5,40,0);
 
 	    System.out.println(sala41.toString());
         System.out.println(sala21.toString());
+
+        rozpocznijWyklad();
+
+        System.out.println(sala41.toString());
+        System.out.println(sala21.toString());
+    }
+
+    public static void rozpocznijWyklad(){
+
     }
 }
