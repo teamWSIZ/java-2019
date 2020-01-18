380a85cb1fd4b0bf65a2b796e83a988899c2627a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-18 14:25:14 +0100

    cd..

--- Zajecia15/src/com/example/objects/Sala.java
+++ Zajecia15/src/com/example/objects/Sala.java
@@ -8,12 +8,23 @@ public class Sala {
     private ArrayList<Krzeslo> mKrzesla = new ArrayList<>();
     private ArrayList<Student> mStudenci = new ArrayList<>();
 
-    public Sala(int ilosc_okien, int krzesla, int komputery){
-        mOkna.add(new Okno(2.5,2.0));
+    public Sala(int ilosc_okien, int krzesla, int komputery) {
+        mOkna.add(new Okno(2.5, 2.0));
     }
 
     @Override
     public String toString() {
-        return "Okna:" + mOkna.size()+" Krzesla: "+mKrzesla.size()+" Komputery: "+mKomputery.size()+" Studenci: "+mStudenci.size();
+        return "Okna:" + mOkna.size()
+                + " Krzesla: " + mKrzesla.size()
+                + " Komputery: " + mKomputery.size() + " Studenci: "
+                + mStudenci.size();
+    }
+
+    public void dodajStudenta(Student student){
+
+    }
+
+    public void dodajGrupe(ArrayList<Student> studenci){
+
     }
 }
