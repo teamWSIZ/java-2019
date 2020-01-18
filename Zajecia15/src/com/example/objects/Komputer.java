a3bb8b0e277f184de90465688e92b5d5b38813a4
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-18 13:22:50 +0100

    cd..

--- Zajecia15/src/com/example/objects/Komputer.java
+++ Zajecia15/src/com/example/objects/Komputer.java
@@ -1,4 +1,22 @@
 package com.example.objects;
 
 public class Komputer {
+    private String name;
+    private Double power;
+
+    private Komputer(){
+    }
+
+    public Komputer(String name, Double power){
+        this.name = name;
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPower() {
+        return power;
+    }
 }
