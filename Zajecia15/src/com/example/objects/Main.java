da104f8a8c30b4fa480d2f99c33ea2b9cd371f0a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-18 14:18:40 +0100

    cd..

--- Zajecia15/src/com/example/objects/Main.java
+++ Zajecia15/src/com/example/objects/Main.java
@@ -3,6 +3,10 @@ package com.example.objects;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Sala sala41 = new Sala(4,40,20);
+	    Sala sala21 = new Sala(4,40,0);
+
+	    System.out.println(sala41.toString());
+        System.out.println(sala21.toString());
     }
 }
---
+++ Zajecia15/src/com/example/objects/Sala.java
@@ -0,0 +1,19 @@
+package com.example.objects;
+
+import java.util.ArrayList;
+
+public class Sala {
+    private ArrayList<Komputer> mKomputery = new ArrayList<>();
+    private ArrayList<Okno> mOkna = new ArrayList<>();
+    private ArrayList<Krzeslo> mKrzesla = new ArrayList<>();
+    private ArrayList<Student> mStudenci = new ArrayList<>();
+
+    public Sala(int ilosc_okien, int krzesla, int komputery){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Okna:" + mOkna.size()+" Krzesla: "+mKrzesla.size()+" Komputery: "+mKomputery.size()+" Studenci: "+mStudenci.size();
+    }
+}
