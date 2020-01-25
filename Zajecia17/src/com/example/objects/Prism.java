5a6ed803cf60720e5eb9b57b79bd67ce5ac57a27
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-25 14:30:25 +0100

    cd..

--- Zajecia17/src/com/example/objects/Prism.java
+++ Zajecia17/src/com/example/objects/Prism.java
@@ -9,11 +9,6 @@ abstract public class Prism {
 
     abstract String getFigureInfo();
 
-    Prism(){
-        evaluateField();
-        evaluateVolume();
-    }
-
     void print(){
         System.out.println(getFigureInfo());
         System.out.println("Pole powierzchni: "+field);
