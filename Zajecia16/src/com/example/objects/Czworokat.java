095fc8faa6fb033c7174a89364caaebeddcf10d7
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-25 12:34:46 +0100

    cd..

---
+++ Zajecia16/src/com/example/objects/Czworokat.java
@@ -0,0 +1,13 @@
+package com.example.objects;
+
+public class Czworokat {
+
+    public double obliczPole(){
+        System.out.println("Figura geometryczna: pole powierzchni nie zostało zdefiniowane");
+        return 0;
+    }
+
+    public void wyswietl(){
+        System.out.println("Pole powierzchni: "+obliczPole());
+    }
+}
--- Zajecia16/src/com/example/objects/Main.java
+++ Zajecia16/src/com/example/objects/Main.java
@@ -1,8 +1,18 @@
 package com.example.objects;
 
+import java.util.ArrayList;
+
 public class Main {
+    static ArrayList<Czworokat> czworokaty = new ArrayList<>();
 
     public static void main(String[] args) {
-	// write your code here
+	    Czworokat czworokat = new Czworokat();
+	    czworokat.obliczPole();
+
+	    for(int i=0;i<10;i++)
+	        czworokaty.add(new Czworokat());
+
+	    for(Czworokat c : czworokaty)
+	        c.wyswietl();
     }
 }
