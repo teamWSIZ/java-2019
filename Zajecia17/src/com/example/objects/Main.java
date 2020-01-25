acf645ef20c63fdc8e6489f22c95627031ea340f
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-25 14:13:07 +0100

    cd..

--- Zajecia17/src/com/example/objects/Main.java
+++ Zajecia17/src/com/example/objects/Main.java
@@ -1,8 +1,11 @@
 package com.example.objects;
 
+import java.util.ArrayList;
+
 public class Main {
+    static ArrayList<Prism> mPrisms = new ArrayList<>();
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
--- Zajecia17/src/com/example/objects/Prism.java
+++ Zajecia17/src/com/example/objects/Prism.java
@@ -1,4 +1,19 @@
 package com.example.objects;
 
-public class Prism {
+abstract public class Prism {
+    double field;
+    double volume;
+
+    abstract void evaluateField();
+    abstract void evaluateVolume();
+
+    Prism(){
+        evaluateField();
+        evaluateVolume();
+    }
+
+    void print(){
+        System.out.println("Pole powierzchni: "+field);
+        System.out.println("Objetosc: "+volume);
+    }
 }
