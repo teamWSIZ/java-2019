ff0e8183f76530c1c68335a3d07b42d99911c807
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-02-01 12:35:59 +0100

    cd..

--- Zajecia17/src/com/example/objects/TrapezoidPrism.java
+++ Zajecia17/src/com/example/objects/TrapezoidPrism.java
@@ -1,18 +1,33 @@
 package com.example.objects;
 
-public class TrapezoidPrism extends Prism{
+public class TrapezoidPrism extends Prism {
+
+    double a, b, h, H;
+    double bottomField;
+    double volume;
+
+    TrapezoidPrism(double a, double b, double h, double H) {
+        this.a = a;
+        this.b = b;
+        this.h = h;
+        this.H = h;
+
+        evaluateBottomField();
+        evaluateVolume();
+    }
+
     @Override
     protected void evaluateBottomField() {
-
+        bottomField = 1.0 / 2.0 * (a + b) / h;
     }
 
     @Override
     protected void evaluateVolume() {
-
+        volume = bottomField * H;
     }
 
     @Override
     String getFigureInfo() {
-        return null;
+        return "Trapezoid Prism";
     }
 }
