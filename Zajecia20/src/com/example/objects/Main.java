6e5a5e4667be650c2293c0e6f6d9b512d9cb8411
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-02-08 14:12:47 +0100

    cd..

--- Zajecia20/src/com/example/objects/Main.java
+++ Zajecia20/src/com/example/objects/Main.java
@@ -2,7 +2,19 @@ package com.example.objects;
 
 public class Main {
 
+    @FunctionalInterface
+    interface dzialania{
+        // a->a
+        // a->{ return a*a; }
+
+        double dzialanie(double x);
+    }
+
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    public static void wyswietlDzialanie(double x, dzialania dz){
+        System.out.println("Wynik działania:"+dz.dzialanie(x));
     }
 }
