7877a89c1b51c176aa294c3a9f062c0c56e610b8
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-02-08 12:45:46 +0100

    cd..

--- Zajecia19/src/com/example/objects/Main.java
+++ Zajecia19/src/com/example/objects/Main.java
@@ -1,8 +1,30 @@
 package com.example.objects;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
 public class Main {
+    static ArrayList<Double> mTablica = new ArrayList<>();
 
     public static void main(String[] args) {
-	// write your code here
+        for(int i=0;i<10;i++)
+            mTablica.add((double)i);
+
+        wyswietlTablice(mTablica);
+    }
+
+    public static void wyswietlTablice(@NotNull ArrayList<Double> t){
+
+        System.out.print("[");
+
+        for(int i=0;i<t.size();i++){
+            if(i<t.size()-1)
+                System.out.print(""+i+", ");
+            else
+                System.out.print(""+i);
+        }
+
+        System.out.println("]");
     }
 }
