a35377be5b135563f566cdedb6a66e382ff3c8b0
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-06 17:27:02 +0100

    cd..

--- Zajecia26/src/sample/Main.java
+++ Zajecia26/src/sample/Main.java
@@ -3,6 +3,8 @@ package sample;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -13,7 +15,14 @@ public class Main extends Application{
         Group group = new Group();
         Scene scene = new Scene(group,512,512);
 
+        primaryStage.setTitle("Zajecia26");
+
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        Rectangle rect = new Rectangle(100,100);
+        rect.setFill(Color.CADETBLUE);
+
+        group.getChildren().add(rect);
     }
 }
