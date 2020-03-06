0c341fbd71c5f3648b800c0f673a7dda5b69bbdd
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-06 17:19:08 +0100

    cd..

--- Zajecia26/src/sample/Main.java
+++ Zajecia26/src/sample/Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -8,5 +10,10 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        Group group = new Group();
+        Scene scene = new Scene(group,512,512);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
