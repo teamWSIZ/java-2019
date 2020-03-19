f0ecdf98b6c2122f5609283f10d387d6e6138c11
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-19 23:43:53 +0100

    Zajecia31

--- Zajecia31/src/main/java/com/javafx/example/App.java
+++ Zajecia31/src/main/java/com/javafx/example/App.java
@@ -4,12 +4,10 @@
 package com.javafx.example;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 public class App extends Application {
