4a0562653486291387b0dae7f9b2b479b374d020
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-28 09:18:14 +0100

    cd..

--- Zajecia34/src/main/java/com/example/exceptions/App.java
+++ Zajecia34/src/main/java/com/example/exceptions/App.java
@@ -4,11 +4,8 @@
 package com.example.exceptions;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        App app = new App();
     }
 }
