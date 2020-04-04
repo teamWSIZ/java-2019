ec1c19e44693f9955670dbeb092a62aa8c4c67c3
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-04 09:14:04 +0200

    cd..

--- Zajecia37/src/main/java/com/enum1/example/App.java
+++ Zajecia37/src/main/java/com/enum1/example/App.java
@@ -4,11 +4,8 @@
 package com.enum1.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        App app = new App();
     }
 }
