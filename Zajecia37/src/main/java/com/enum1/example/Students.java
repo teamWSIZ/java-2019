3528e314d0d76c362f2537f3bc279a1ebc287179
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-17 17:23:13 +0200

    cd..

--- Zajecia37/src/main/java/com/enum1/example/Students.java
+++ Zajecia37/src/main/java/com/enum1/example/Students.java
@@ -1,11 +1,11 @@
 package com.enum1.example;
 
 public enum Students {
-    KRZYSZTOF_ZAREBSKI(183),
-    JOZEF_KUDRYS(180),
-    KAMIL_STASICA(176),
-    ARKADIUSZ_KUBINSKI(192),
-    KATARZYNA_SKRZYP(168);
+    KRZYSZTOF_ZAREBSKI(183,10),
+    JOZEF_KUDRYS(180,10),
+    KAMIL_STASICA(176,10),
+    ARKADIUSZ_KUBINSKI(192,10),
+    KATARZYNA_SKRZYP(168,10);
 
     final private double height;
     final private int maxPullUp;
@@ -15,8 +15,9 @@ public enum Students {
         groupInfo = "Gr.C";
     }
 
-    Students(double height,double maxPullUp){
+    Students(double height,int maxPullUp){
         this.height = height;
+        this.maxPullUp = maxPullUp;
     }
 
     public double getHeight() {
