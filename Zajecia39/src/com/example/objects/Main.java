7182106c9148e0a7c28216840e10297156c25efd
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-18 09:10:51 +0200

    cd..

--- Zajecia39/src/com/example/objects/Main.java
+++ Zajecia39/src/com/example/objects/Main.java
@@ -2,28 +2,28 @@ package com.example.objects;
 
 import java.util.ArrayList;
 
-enum Month{
-    Styczen, Luty, Marzec, Kwiecien,Maj,Czerwiec;
+enum Month {
+    Styczen, Luty, Marzec, Kwiecien, Maj, Czerwiec;
 }
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         var array1 = new ArrayList<Double>();
         var a = 10.0;
 
         Main main = new Main();
 
         Month month = Month.Kwiecien;
-        System.out.println(main.season1(month));
+        System.out.println(main.season2(month));
 
     }
 
-    void season(Month month){
+    void season(Month month) {
         String nameOfSeason = "";
 
-        switch(month){
+        switch (month) {
             case Styczen:
             case Luty:
                 nameOfSeason = "Zima";
@@ -40,14 +40,27 @@ public class Main {
         System.out.println(nameOfSeason);
     }
 
-    String season1(Month month){
+    String season1(Month month) {
         String nameOfSeason = "";
 
-        switch(month){
+        switch (month) {
             case Styczen, Luty -> nameOfSeason = "Zima";
             case Marzec, Kwiecien, Maj -> nameOfSeason = "Wiosna";
             default -> nameOfSeason = "Inna pora roku";
         }
+
+        //System.out.println(nameOfSeason);
+        return nameOfSeason;
+    }
+
+    String season2(Month month) {
+        String nameOfSeason =
+
+                switch (month) {
+                    case Styczen, Luty -> "Zima";
+                    case Marzec, Kwiecien, Maj -> "Wiosna";
+                    default -> "Inna pora roku";
+                };
 
         //System.out.println(nameOfSeason);
         return nameOfSeason;
