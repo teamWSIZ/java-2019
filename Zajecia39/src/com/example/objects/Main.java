a0c003b2354c5a601dcd4a49c68063d8678dbab6
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-18 08:54:26 +0200

    cd..

--- Zajecia39/src/com/example/objects/Main.java
+++ Zajecia39/src/com/example/objects/Main.java
@@ -1,8 +1,35 @@
 package com.example.objects;
 
+import java.util.ArrayList;
+
+enum Month{
+    Styczen, Luty, Marzec, Kwiecien,Maj,Czerwiec;
+}
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        var array1 = new ArrayList<Double>();
+        var a = 10.0;
+
+        Main main = new Main();
+
+        Month month = Month.Kwiecien;
+        System.out.println(main.season(month));
+    }
+
+    String season(Month month){
+        switch(month){
+            case Styczen:
+            case Luty:
+                return "Zima";
+            case Marzec:
+            case Kwiecien:
+            case Maj:
+                return "Wiosna";
+            default:
+                return "Inna pora roku";
+        }
     }
 }
