a2257590bd74cff9ca1fbdce1bf33a1eb85bc9fd
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-24 16:34:44 +0200

    cd..

--- Zajecia40/src/com/example/objects/Main.java
+++ Zajecia40/src/com/example/objects/Main.java
@@ -1,8 +1,12 @@
 package com.example.objects;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        var array = Arrays.asList(20,1,5,3,5,6,7,8,10);
+        System.out.println(array);
     }
 }
