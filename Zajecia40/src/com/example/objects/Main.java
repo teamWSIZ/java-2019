3d8485dd972db4da7e36e554ea9090ed82e92860
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-24 17:27:54 +0200

    cd..

--- Zajecia40/src/com/example/objects/Main.java
+++ Zajecia40/src/com/example/objects/Main.java
@@ -44,7 +44,7 @@ public class Main {
             System.out.println("------------------");
         }
 
-        System.out.println("Przestawienia: "+noIteration);
+        System.out.println("Liczba wejść do pętli: "+noIteration);
 
     }
 
