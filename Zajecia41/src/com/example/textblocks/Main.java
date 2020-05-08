1d9e289dca1271fc3f36efef03ac8a842e56d8b9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-05-08 17:24:44 +0200

    cd..

--- Zajecia41/src/com/example/textblocks/Main.java
+++ Zajecia41/src/com/example/textblocks/Main.java
@@ -3,6 +3,16 @@ package com.example.textblocks;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String simpleText = "Pierwsza linia tekstu\n"
+                +"Kolejna linia tekstu";
+
+        String blockText = """
+                Pierwsza linia tekstu
+                Kolejna linia tekstu
+                """;
+
+        System.out.println(simpleText);
+        System.out.println(blockText);
     }
 }
