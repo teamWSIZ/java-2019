28db41e5fb82e9461c03d1f8abf9aab82cf8e414
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-05-09 08:53:45 +0200

    cd..

--- Zajecia42/src/com/example/innerclass/Main.java
+++ Zajecia42/src/com/example/innerclass/Main.java
@@ -2,7 +2,33 @@ package com.example.innerclass;
 
 public class Main {
 
+    class OuterClass{
+        class ClassA{
+            @Override
+            public String toString() {
+                return "ClassA";
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "OuterClass";
+        }
+    }
+
     public static void main(String[] args) {
 	// write your code here
+        Main main = new Main();
+        main.example();
+    }
+
+    void example(){
+        OuterClass outerClass = new OuterClass();
+        var outerClass1 = new OuterClass();
+
+        System.out.println(outerClass1);
+
+        OuterClass.ClassA classA = outerClass.new ClassA();
+        System.out.println(classA);
     }
 }
