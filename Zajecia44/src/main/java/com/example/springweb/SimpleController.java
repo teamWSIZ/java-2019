4ec629d0f8eb3c5edfa4ff890fa043ac8f9e5b52
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-05-29 17:22:08 +0200

    cd..

--- Zajecia44/src/main/java/com/example/springweb/SimpleController.java
+++ Zajecia44/src/main/java/com/example/springweb/SimpleController.java
@@ -2,18 +2,25 @@ package com.example.springweb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
     @RequestMapping("/")
-    String getInfo(){
+    String getInfo() {
         return "info";
     }
 
-    @RequestMapping("number")
-    Double getNumber(){
-        return 20.0;
+    //http://localhost:8080/equation?A=40&B=5&C=2&x=2
+
+    @RequestMapping("equation")
+    Double getNumber(@RequestParam(value = "A", defaultValue = "2.0") Double a,
+                     @RequestParam(value = "B", defaultValue = "2.0") Double b,
+                     @RequestParam(value = "C", defaultValue = "2.0") Double c,
+                     @RequestParam(value = "x", defaultValue = "1.0") Double x) {
+        //y=a*x^2+bx+c
+        return a * x + x + b * x + c;
     }
 }
