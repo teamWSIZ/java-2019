b9315dd3ce80f4777f78740243ffae5d8cee0cfa
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-05-30 08:32:07 +0200

    Zajecia45

---
+++ Zajecia45/src/main/java/com/example/simplehtml/Zajecia45Application.java
@@ -0,0 +1,13 @@
+package com.example.simplehtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zajecia45Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zajecia45Application.class, args);
+	}
+
+}
---
+++ Zajecia45/src/test/java/com/example/simplehtml/Zajecia45ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplehtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zajecia45ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
