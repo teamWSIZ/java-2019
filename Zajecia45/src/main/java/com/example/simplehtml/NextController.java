cb5ede138b4d1895397b5606ebdafb80293b2e73
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-05-30 09:37:03 +0200

    cd..

--- Zajecia45/src/main/java/com/example/simplehtml/NextController.java
+++ Zajecia45/src/main/java/com/example/simplehtml/NextController.java
@@ -1,14 +1,20 @@
 package com.example.simplehtml;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class NextController {
-    @RequestMapping("index")
-    String getHtml(){
+    @RequestMapping("student")
+    String getHtml(@RequestParam(value = "name", defaultValue = "Przemysław") String name,
+                   @RequestParam(value = "surname", defaultValue = "Stokłosa") String surname,
+                   Model model){
+
+        model.addAttribute("name",name);
+        model.addAttribute("surname",surname);
+
         return "page0";
     }
 }
