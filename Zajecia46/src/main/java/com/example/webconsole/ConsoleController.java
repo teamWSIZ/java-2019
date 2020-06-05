cbcef2e0bd9212e8258afa0a4066f99b6ff6240e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-05 17:33:48 +0200

    Zajecia46

--- Zajecia46/src/main/java/com/example/webconsole/ConsoleController.java
+++ Zajecia46/src/main/java/com/example/webconsole/ConsoleController.java
@@ -3,10 +3,19 @@ package com.example.webconsole;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 @Controller
 public class ConsoleController {
+    ArrayList<String> consoleText = new ArrayList<>();
+
     @RequestMapping("")
     String getConsole(){
         return "console";
+    }
+
+    void example(){
+        //System.out.println("Single line")
+        consoleText.add("Single line");
     }
 }
