420ea42b01c6dda78f0b98e63e9850a7bec9d04d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-06 09:22:30 +0200

    cd..

--- Zajecia47/src/com/example/threads/Main.java
+++ Zajecia47/src/com/example/threads/Main.java
@@ -1,17 +1,22 @@
 package com.example.threads;
 
+import java.util.ArrayList;
+
 class Worker implements Runnable{
 
     private static int no = 0;
+    private int mNo;
 
     Worker(){
+        mNo = no;
         no++;
     }
 
     @Override
     public void run() {
-        for(int i=0;i<100;i++)
-            System.out.println("Worker("+no+"): i="+i);
+        for(int i=0;i<100;i++) {
+            System.out.println("Worker(" + mNo + "): i=" + i);
+        }
     }
 }
 
@@ -19,10 +24,19 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        for(int i=0;i<10;i++) {
+        /*for(int i=0;i<10;i++) {
             Worker worker = new Worker();
             worker.run();
+        }*/
+
+        ArrayList<Thread> threadArray = new ArrayList<>();
+
+        for(int i=0;i<10;i++){
+            Thread thread = new Thread(new Worker());
+            threadArray.add(thread);
         }
+
+        threadArray.forEach(Thread::start);
 
     }
 }
