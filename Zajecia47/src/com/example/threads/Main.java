96f69c47be049e90d148f9a9d9696390989f0406
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-06 08:58:05 +0200

    cd..

--- Zajecia47/src/com/example/threads/Main.java
+++ Zajecia47/src/com/example/threads/Main.java
@@ -2,29 +2,27 @@ package com.example.threads;
 
 class Worker implements Runnable{
 
+    private static int no = 0;
+
+    Worker(){
+        no++;
+    }
+
     @Override
     public void run() {
         for(int i=0;i<100;i++)
-            System.out.println("Worker(1): i="+i);
+            System.out.println("Worker("+no+"): i="+i);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
 	// write your code here
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                for(int i=0;i<10;i++) {
-                    System.out.println("i="+i);
-                }
-            }
-        };
 
-        runnable.run();
-
-        Worker worker = new Worker();
-        worker.run();
+        for(int i=0;i<10;i++) {
+            Worker worker = new Worker();
+            worker.run();
+        }
 
     }
 }
