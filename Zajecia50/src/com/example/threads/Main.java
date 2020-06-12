cbfb156dd8a2aae51318c01f589f1689feaa67ad
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-12 17:21:13 +0200

    cd..

---
+++ Zajecia50/src/com/example/threads/Main.java
@@ -0,0 +1,16 @@
+package com.example.threads;
+
+import java.util.concurrent.Callable;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    Callable<Double> squareEquation(double A, double B, double C){
+        return ()->{
+            return 0.0;
+        };
+    }
+}
