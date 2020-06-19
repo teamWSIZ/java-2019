78df6617bfc089c5b58bc50db3fc2b3e548778b9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-19 16:55:29 +0200

    cd..

--- Zajecia51/src/com/example/threads/Main.java
+++ Zajecia51/src/com/example/threads/Main.java
@@ -1,8 +1,35 @@
 package com.example.threads;
 
+import java.util.ArrayDeque;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class ChatLine{
+    ChatLine(String text){
+
+    }
+    @Override
+    public String toString() {
+        return "ChatLine{}";
+    }
+}
+
 public class Main {
+    ArrayDeque<ChatLine> chatDeque;
 
     public static void main(String[] args) {
 	// write your code here
+        ExecutorService chatService = Executors.newWorkStealingPool();
+
+        Future<ChatLine> future0 = chatService.submit(createUser("Name"));
+
+    }
+
+    static Callable<ChatLine> createUser(String param){
+        return ()->{
+            return new ChatLine("Zakończono: "+param);
+        };
     }
 }
