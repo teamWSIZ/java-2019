cbf02261f0bafa340ed500034ebcdf1f593cab9d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-19 16:58:34 +0200

    cd..

--- Zajecia51/src/com/example/threads/Main.java
+++ Zajecia51/src/com/example/threads/Main.java
@@ -1,14 +1,17 @@
 package com.example.threads;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayDeque;
+import java.util.Date;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 class ChatLine{
+    final Date date;
     ChatLine(String text){
-
+        date = new Date();
     }
     @Override
     public String toString() {
@@ -22,14 +25,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         ExecutorService chatService = Executors.newWorkStealingPool();
-
-        Future<ChatLine> future0 = chatService.submit(createUser("Name"));
+        Future<ChatLine> future0 = chatService.submit(createUser("Name","Surname"));
 
     }
 
-    static Callable<ChatLine> createUser(String param){
+    static Callable<ChatLine> createUser(String name, String surname){
         return ()->{
-            return new ChatLine("Zakończono: "+param);
+            return new ChatLine("Zakończono: ");
         };
     }
 }
