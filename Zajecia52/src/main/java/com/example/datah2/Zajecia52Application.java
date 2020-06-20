2ffcf248054d244d13d83c1bceb2ce92c1e06f87
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-20 09:15:32 +0200

    Zajecia52

---
+++ Zajecia52/src/main/java/com/example/datah2/Zajecia52Application.java
@@ -0,0 +1,13 @@
+package com.example.datah2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zajecia52Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zajecia52Application.class, args);
+	}
+
+}
---
+++ Zajecia52/src/test/java/com/example/datah2/Zajecia52ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zajecia52ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
