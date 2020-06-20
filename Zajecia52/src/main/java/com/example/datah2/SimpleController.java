b21e96d127acf9304f1fc84c215466063c799c3e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-20 09:42:17 +0200

    cd..

--- Zajecia52/src/main/java/com/example/datah2/SimpleController.java
+++ Zajecia52/src/main/java/com/example/datah2/SimpleController.java
@@ -1,19 +1,28 @@
 package com.example.datah2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SimpleController {
+    @Autowired
+    StudentRepository repository;
+
     @RequestMapping("add")
     @ResponseBody
-    String addStudent(){
+    String addStudent(@RequestParam(value = "name",defaultValue = "Przemysław") String name, @RequestParam(value = "surname", defaultValue = "Stokłosa") String surname){
+        repository.save(new Student(name,surname));
         return "Dodano poprawnie";
     }
 
     @RequestMapping("")
-    String listAllStudents(){
+    String listAllStudents(Model model){
+        Iterable<Student> students = repository.findAll();
+        model.addAttribute("students",students);
         return "listStudents";
     }
 }
---
+++ Zajecia52/src/main/java/com/example/datah2/StudentRepository.java
@@ -0,0 +1,6 @@
+package com.example.datah2;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepository extends CrudRepository<Student,Integer> {
+}
