45c2a3c14b6ae4cf70e5cd46a1365a8b2d309dfd
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-26 16:52:22 +0200

    cd..

--- Zajecia53/src/com/example/generics/functions/Main.java
+++ Zajecia53/src/com/example/generics/functions/Main.java
@@ -1,23 +1,47 @@
 package com.example.generics.functions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class MyClass<T>{
-    ArrayList<T> array;
-    MyClass(ArrayList<T> array){
+    List<T> array;
+    MyClass(){
+    }
+    MyClass(List<T> array){
         this.array = array;
     }
 
-    void printArray(ArrayList<T> data){
+    void printArray(List<T> data){
+        data.forEach(System.out::println);
+    }
+
+    <U> void nextPrint(List<U> data){
         data.forEach(System.out::println);
     }
 }
 
-
 public class Main {
 
     public static void main(String[] args) {
+        Main main = new Main();
+        main.example();
     }
+
+    void example(){
+        MyClass<Double> myClass = new MyClass<>();
+        List<Integer> array1 = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<Double> array2 = Arrays.asList(1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0);
+
+        myClass.printArray(array2);
+
+        //To nie będzie działało
+        //myClass.printArray(array1);
+
+        myClass.nextPrint(array1);
+        myClass.nextPrint(array1);
+    }
+
 
 
 }
