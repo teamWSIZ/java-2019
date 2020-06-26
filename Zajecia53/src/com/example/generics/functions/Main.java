899064353d1262fbfdbb43b738ffb72b8c8ea525
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-26 16:38:16 +0200

    cd..

--- Zajecia53/src/com/example/generics/functions/Main.java
+++ Zajecia53/src/com/example/generics/functions/Main.java
@@ -1,8 +1,23 @@
 package com.example.generics.functions;
 
+import java.util.ArrayList;
+
+class MyClass<T>{
+    ArrayList<T> array;
+    MyClass(ArrayList<T> array){
+        this.array = array;
+    }
+
+    void printArray(ArrayList<T> data){
+        data.forEach(System.out::println);
+    }
+}
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
     }
+
+
 }
