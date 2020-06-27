a590a7b331d96fae89d81424b91853df850e2a71
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-27 09:28:46 +0200

    cd..

--- Zajecia52/src/main/java/com/example/datah2/DatabaseApplication.java
+++ Zajecia52/src/main/java/com/example/datah2/DatabaseApplication.java
@@ -16,7 +16,7 @@ public class DatabaseApplication {
 	@Bean
 	CommandLineRunner addStudents(StudentRepository repository){
 		return (args)->{
-			for(int i=0;i<10000;i++) {
+			for(int i=0;i<100;i++) {
 				repository.save(new Student("Przemysław", "Stokłosa" + i));
 				repository.save(new Student("Piotr", "Stokłosa" + i));
 			}
