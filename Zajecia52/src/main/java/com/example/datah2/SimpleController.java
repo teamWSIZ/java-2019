a513b74200adba8578f399cc5e54382543527328
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-27 09:40:33 +0200

    cd..

--- Zajecia52/src/main/java/com/example/datah2/SimpleController.java
+++ Zajecia52/src/main/java/com/example/datah2/SimpleController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class SimpleController {
     @Autowired
@@ -22,6 +24,12 @@ public class SimpleController {
     @RequestMapping("")
     String listAllStudents(Model model){
         Iterable<Student> students = repository.findAll();
+        model.addAttribute("students",students);
+        return "listStudents";
+    }
+    @RequestMapping("findName")
+    String listStudentsName(@RequestParam(value = "name", defaultValue = "Piotr") String name, Model model){
+        List<Student> students = repository.findStudentByName(name);
         model.addAttribute("students",students);
         return "listStudents";
     }
--- Zajecia52/src/main/java/com/example/datah2/StudentRepository.java
+++ Zajecia52/src/main/java/com/example/datah2/StudentRepository.java
@@ -2,5 +2,8 @@ package com.example.datah2;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends CrudRepository<Student,Integer> {
+    List<Student> findStudentByName(String name);
 }
