9ebe166a2c17916a7cdc8da03b70ebf6631645d0
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-27 09:28:29 +0200

    cd..

--- Zajecia52/src/main/java/com/example/datah2/DatabaseApplication.java
+++ Zajecia52/src/main/java/com/example/datah2/DatabaseApplication.java
@@ -1,7 +1,9 @@
 package com.example.datah2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DatabaseApplication {
@@ -9,6 +11,16 @@ public class DatabaseApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(DatabaseApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner addStudents(StudentRepository repository){
+		return (args)->{
+			for(int i=0;i<10000;i++) {
+				repository.save(new Student("Przemysław", "Stokłosa" + i));
+				repository.save(new Student("Piotr", "Stokłosa" + i));
+			}
+		};
 	}
 
 }
