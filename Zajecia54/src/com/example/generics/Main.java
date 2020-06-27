8a2fae2111997c87028164ddc2e0af82a10911a0
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-27 08:37:14 +0200

    cd..

--- Zajecia54/src/com/example/generics/Main.java
+++ Zajecia54/src/com/example/generics/Main.java
@@ -1,8 +1,28 @@
 package com.example.generics;
 
+class A{
+
+}
+class B extends A{
+
+}
+class B1 extends A{
+
+}
+class C extends B{
+
+}
+class D extends C{
+
+}
+class E extends D{
+
+}
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //A->B->C->D-E
+        //A->B1->C->D-E
     }
 }
