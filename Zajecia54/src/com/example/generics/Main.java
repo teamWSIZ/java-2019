a5ade990f43b0876f2c64a8e77f8b8d9f9225fcc
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-27 08:49:15 +0200

    cd..

--- Zajecia54/src/com/example/generics/Main.java
+++ Zajecia54/src/com/example/generics/Main.java
@@ -1,5 +1,9 @@
 package com.example.generics;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
 class A{
 
 }
@@ -10,7 +14,9 @@ class B1 extends A{
 
 }
 class C extends B{
-
+    void print(){
+        System.out.println("Tutaj pojawiła się nowa funkcja");
+    }
 }
 class D extends C{
 
@@ -27,12 +33,26 @@ public class Main {
     }
 
     void example(){
-        //A->B->C->D-E
-        //A->B1->C->D-E
+        //A->B->C->D->E
+        //A->B1->C->D->E
+        C c = new C();
+        c.print();
+        D d = new D();
+        d.print();
+
+        ArrayList<A> arrayA = new ArrayList<>();
+        // to nie zadziała funE(arrayA);
+
+        ArrayList<C> array = new ArrayList<>();
+        ArrayList<D> array1 = new ArrayList<>();
+        funE(array);
+        funE(array1);
     }
 
-    void funE(){
-
+    void funE(@NotNull ArrayList<? extends C> data){
+        //A->B->C->D->E
+        //Działa od tego momentu: C->D->E, tutaj pojawiła się funkcja print
+        data.forEach(C::print);
     }
     void funS(){
 
