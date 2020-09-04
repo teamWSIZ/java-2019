38d5432661f13417374bf6061a8dc007cab73cf5
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-09-04 16:56:42 +0200

    cd..

--- Zajecia55/src/com/example/streams/Main.java
+++ Zajecia55/src/com/example/streams/Main.java
@@ -1,8 +1,23 @@
 package com.example.streams;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        ArrayList<Integer> array = new ArrayList<>();
+        for(int i=0;i<10;i++)
+            array.add(i);
+
+        System.out.println(array);
+
+        ArrayList<Integer> array1 = new ArrayList<>();
+
+        for(Integer element : array){
+            array1.add(element*element);
+        }
+
+        System.out.println(array1);
     }
 }
