c4853b193ebc406aee84ee5aa6cc6bd0ed925378
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-09-05 09:10:33 +0200

    Zajecia56

--- Zajecia56/src/com/example/streams/Main.java
+++ Zajecia56/src/com/example/streams/Main.java
@@ -1,5 +1,7 @@
 package com.example.streams;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,7 +15,19 @@ public class Main {
 
         Stream<Integer> stream = list.stream().map(a->a*a);
 
-        List<Integer> list1 = stream.collect(Collectors.toList());
-        System.out.println(list1);
+        //List<Integer> list1 = stream.collect(Collectors.toList());
+        //System.out.println(list1);
+
+        ArrayList<Integer> array = stream.collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(array);
+
+        Main main = new Main();
+        main.example1();
+    }
+
+    void example1(){
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        ArrayList<Integer> array = list.stream().map(a->a*a*a).collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(array);
     }
 }
